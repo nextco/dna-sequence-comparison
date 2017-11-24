847269cfeab1e455df5a3511050a50c07e939c29
Roy H <maro@nextco.me>
2017-11-24 15:57:30 -0500

    Algunos bugs sueltos, agregado alineamiento global & local

--- la.cpp
+++ la.cpp
@@ -1,10 +1,12 @@
-// Alineamiento Global
+// Bug lcs.data
+// Alineamiento Local
 #include <iostream> // cout 
 #include <algorithm> // max
 #include <random> //
 #include <string> // stoi
 #include <chrono> // diferencia de tiempo 
 #include <vector> // struct strip
+#include <limits> // Maximo & minimo valor de int
 
 using namespace std;
 
@@ -132,6 +134,7 @@ string obtener_char(char *&c, int posicion){
 	return s;
 }
 
+// Devuelve true si dos caracteres son iguales
 bool comparar_char(char *&v, char *&w, int i, int j){
 	bool equals = false;
 
@@ -187,12 +190,12 @@ void calcular_similaridad(char *&v, char *&w, int n, int m, int g, int **& p, in
 	
 	// Linea 3
 	for(int i = 0; i < columnas; i++){
-       	S[0][i] = i*g;
+       	S[0][i] = 0; // Fix
     }
 
     // Linea 5
 	for(int j = 1; j < filas; j++){
-       	S[j][0] = j*g;
+       	S[j][0] = 0; // Fix
     }
 
     /*
@@ -210,9 +213,10 @@ void calcular_similaridad(char *&v, char *&w, int n, int m, int g, int **& p, in
 			
 			int a = S[i-1][j] + g;
 			int b = S[i-1][j-1] + p[i][j];
-			int c = S[i][j-1] + g;			
+			int c = S[i][j-1] + g;
+			int d = 0; // Aplicacion			
 
-			S[i][j] = maximo( a, maximo(b, c) );
+			S[i][j] = maximo( maximo(a, b), maximo(c, d) );
 	    }
     }
 	
@@ -221,42 +225,71 @@ void calcular_similaridad(char *&v, char *&w, int n, int m, int g, int **& p, in
 
 }
 
-// Pag 177
-void alineamiento_global(int **& S, int i , int j, int L, char *&v, char *&w, int **& p, int g,
-	string &alinea_vl, string &alinea_wl){
-	// cout << "ag -> " << i << ", " << j << endl; 
-	if( i == 0 && j == 0){
-		L = 0;
+void maximo_score(int **&S, int &score_maximo_i, int &score_maximo_j, int i, int j){	
+	int max_score = numeric_limits<int>::min(); // minimo valor que puede tomar int
+	// cout << "max_score: " << max_score << endl;
+	for (int k = 1; k != i ; k++){
+		for (int l = 1; l != j ; l++){
+			// cout << "(k, l) => " << k << ", " << l << " = " << S[k][l]<< endl;
+			if( S[k][l] >= max_score){
+				// cout << "(k, l) => " << k << ", " << l << " = " << S[k][l]<< endl;
+				max_score = S[k][l];
+				score_maximo_i = k;
+				score_maximo_j = l;
+			}
+		}
+	}
+	// cout << "max_score: " << max_score << endl; 
+}
+
+void obtener_cadena_alineamiento_local(int **&S, int i, int j, string &max_secuencia, char *&v){
+
+	// cout << "obtener_cadena_alineamiento_local: " << i << ", "<< j << " = "<< S[i][j] << endl;
+	
+	// Hasta encontrar un cero - Fixed
+	if( S[i][j] == 0 ){				
 		return;
-    }else if(i > 0 && ( S[i][j] == S[i-1][j] + g) ){
-    	alineamiento_global(S, i-1, j, L, v, w, p, g, alinea_vl, alinea_wl);
-    	L++;
+	}
 
-    	cout << "vl: "<< v[i-1];
-    	cout << ", wl: "<< '-' << endl;
+	obtener_cadena_alineamiento_local(S, i-1, j-1, max_secuencia, v);
+    // cout << "Sequencia: "<< v[i - 1] << endl;
+    max_secuencia.push_back( v[i - 1] );
+}
 
-    	alinea_wl.push_back( v[i - 1] );
-    	alinea_wl.push_back( '-' );
+// c = a + b
+void sumar_matrix(int **&a, int **&b, int **&c, int filas, int columnas){
+    cout << "sumar_matrix -> " << endl;
 
-    }else if( i > 0 && j > 0 && ( S[i][j] == S[i-1][j-1] + p[i][j]) ){
-    	alineamiento_global(S, i-1, j-1, L, v, w, p, g, alinea_vl, alinea_wl);
-    	L++;
-    	
-    	cout << "vl: "<< v[i-1];
-    	cout << ", wl: "<< w[j-1] << endl;
+    init_matrix(c, filas, columnas );    
+    
+	for(int i = 0; i < filas; i++) {
+		for(int j = 0; j < columnas; j++) {
+			c[i][j] = a[i][j]  + b[i][j];
+		}
+	}
 
-    	alinea_vl.push_back( v[i - 1] );
-    	alinea_wl.push_back( w[j - 1] );
+}
 
-    }else if( j > 0 && ( S[i][j] == S[i][j-1] + g) ){
-    	alineamiento_global(S, i, j-1, L, v, w, p, g, alinea_vl, alinea_wl);
-    	L++;
+void alineamiento_local(int **& S, int filas , int columnas, char *&v, char *&w, int **& p, int g,
+	string &max_secuencia){
 
-    	cout << "vl: "<< '-';
-    	cout << ", wl: "<< w[j-1] << endl;
-    	alinea_vl.push_back( '-' );
-    	alinea_wl.push_back( w[j - 1] );
-    }
+	cout << "alineamiento_local -> " << filas << ", " << columnas << endl;
+	
+	// Buscar maximo score en matriz
+	int score_maximo_i = 0;
+	int score_maximo_j = 0;
+
+	maximo_score(S, score_maximo_i, score_maximo_j, filas, columnas);
+	
+	cout << "score_maximo_i: "<< score_maximo_i;
+    cout << ", score_maximo_j: "<< score_maximo_j;
+    cout << "=> max_score: " << S[score_maximo_i][score_maximo_j] << endl;
+
+
+    obtener_cadena_alineamiento_local(S, score_maximo_i, score_maximo_j, max_secuencia, v);
+
+    // cout << "=> max_secuencia: " << max_secuencia << endl;
+
 }
 
 
@@ -277,6 +310,53 @@ void obtener_indels(string &alinea_vl, int &inserciones, string &alinea_wl, int 
 		}
 	}
 	
+}
+
+void obtener_alineacion(char *&v, int n, char *&w, int m, string &alinea_vl, string &alinea_wl, string &max_secuencia){
+	cout << "obtener_alineacion -> " << endl;
+	alinea_vl = ""; alinea_wl = "";
+
+	int i = 0;
+	int j = 0;
+
+	// Posible restricción cadenas de igual tamaño
+	while(i != n){
+
+		// cout << endl <<  v[i] << ", " << w[j] << "=> "<< comparar_char(v, w, i, j)<< endl;
+
+		if( comparar_char(v, w, i, j) ){ // Son iguales
+			alinea_vl.push_back( v[i] );
+			alinea_wl.push_back( w[j] );
+
+			// cout << "vl: " << alinea_vl << endl;
+			// cout << "wl: " << alinea_wl;
+			i++; // Avanzar ambos
+			j++;
+
+		}else if( !comparar_char(v, w, i, j) && j < m ) { // No Son iguales			
+			alinea_vl.push_back( '-' );
+			alinea_wl.push_back( w[j] );
+
+			// cout << "vl: " << alinea_vl << endl;
+			// cout << "wl: " << alinea_wl;
+
+			j++; // Avanzar w
+		}else{
+			alinea_vl.push_back( v[i]);
+			alinea_wl.push_back( '-' );
+
+			// cout << "vl: " << alinea_vl << endl;
+			// cout << "wl: " << alinea_wl;
+
+			i++;// Avanzar i
+		}		
+	
+	}
+
+	// cout << endl << "vl: " << alinea_vl << endl;
+	// cout << "wl: " << alinea_wl;
+	
+
 }
 
 
@@ -305,20 +385,29 @@ int main(int argc, const char* argv[]) {
 	imprimir_matriz_header_s(matrix_p, n+1, m+1, v, w );
 
   	cout << endl << "S: " << endl;
-	imprimir_matriz_header_s(matrix_s, n+1, m+1, v, w );	
-	
-	int L = 0; // Longitud de alineamiento ?
-	string alinea_vl;
-	string alinea_wl;
-	// 
+	imprimir_matriz_header_s(matrix_s, n+1, m+1, v, w );
 	cout << endl;
-	alineamiento_global(matrix_s, n , m, L, v, w, matrix_p, g, alinea_vl, alinea_wl); cout << endl;
+	// 13 Elementos => Matriz 14 Elementos
 	
+	string max_secuencia = ""; // secuencia maxima de alineamiento
+	alineamiento_local(matrix_s, n + 1 , m + 1, v, w, matrix_p, g, max_secuencia); cout << endl;
+	cout << "=> max_secuencia: " << max_secuencia << endl;
+	cout << "v: "; imprimir_chars(v, n); cout << "=> "<< n << endl;
+	cout << "w: "; imprimir_chars(w, m); cout << "=> "<< m << endl;
+
 	// Interpretacion
+	string alinea_vl, alinea_wl;
+	obtener_alineacion(v, n, w, m, alinea_vl, alinea_wl, max_secuencia);
+
+	cout << endl << "vl: " << alinea_vl << endl;
+	cout << "wl: " << alinea_wl;
+
+	
 	int inserciones, eliminaciones;
+
 	obtener_indels(alinea_vl, inserciones, alinea_wl, eliminaciones);
 
-	cout << endl << "Resultados de Alineamiento Global e Interpretacion; para transformar v en w se requieren: "<< endl;
+	cout << endl << endl << "Resultados de Alineamiento Local e Interpretacion; para transformar v en w se requieren: "<< endl;
 	cout << "alinea_vl: " << alinea_vl << "=> " << inserciones << " inserciones" << endl;
 	cout << "alinea_wl: " << alinea_wl << "=> " << eliminaciones <<" eliminaciones" << endl;
 
